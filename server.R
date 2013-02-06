c2d861ee6aba8b444bab77d089ee45d1d9602b5c
Gregory Jefferis <jefferis@gmail.com>
2013-02-06 22:44:41 +0000

    First functional version that actually does some transforms

--- server.R
+++ server.R
@@ -1,7 +1,22 @@
 library(shiny)
 
+# registrations<-dir("")
+source(getOption('gjanalysissuite.startup'))
+ 
 shinyServer(function(input, output) {
-
+    
+    select_transform<-reactive(function(){
+        RegDir=file.path(dirname(attr(body(function() {}),'srcfile')$filename),'BridgingRegistrations')
+        reg=file.path(RegDir,paste(input$to,sep="","_",input$from,".list"))
+        if(file.exists(reg)) return(reg)
+        else {
+            return(paste("Reg not found:",reg))
+        }
+    })
+    output$regpath<-reactiveText(function(){
+        select_transform()
+    })
+    
     xformed_points <- reactive(function() {
         pts=read.table(text=input$input_points)
         if(ncol(pts)==3) {
@@ -11,11 +26,18 @@ shinyServer(function(input, output) {
         } else {
             stop("Data must have 3 or 4 columns")
         }
-        pts
+        reg=select_transform()
+        if(!is.na(reg) || input$to==input$from){
+            xpts=transformedPoints(xyzs=pts[,1:3],warpfile=reg,transforms='warp',gregxoptions='')$warp
+            if(ncol(pts)==4)
+            xpts=cbind(xpts,pts[,4])
+        } else xpts=pts
+        xpts
       })
-    output$scriptloc <-reactiveText(function() {
-        dirname(attr(body(function() {}),'srcfile')$filename)})
-    
+      
+    # output$scriptloc <-reactiveText(function() {
+    #     dirname(attr(body(function() {}),'srcfile')$filename)})
+    # 
 
     output$view <- reactiveTable(function() {
         head(xformed_points(),5)
--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ templateList=list("T1 (Yu et al 2010, Dickson, IMP)" = "T1",
        "IS2 (Cachero, Ostrovsky et al 2010, Jefferis, MRC LMB)" = "IS2", 
        "Cell07 (Jefferis, Potter 2007, Jefferis/Luo, Cambridge/Stanford)" = "Cell07",
        "FCWB (Ostrovsky/Costa in prep, Jefferis, MRC LMB)" = "FCWB",
-       "JFRC (HHMI/VFB)" = "JFRC")
+       "JFRC2 (HHMI/VFB)" = "JFRC2")
 
 # Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
@@ -22,12 +22,12 @@ shinyUI(pageWithSidebar(
     ),
 
   mainPanel(
-      textOutput("scriptloc"),
       h4("Input points:"),
       HTML('<textarea id="input_points" rows="16" cols=60>1 2 3</textarea>'),
       # textInput('', '3D coords (columnwise)', value = "1 2 3"),
       h4("Transformation:"),
       textOutput('xform'),
+      textOutput("regpath"),
       h4("Transformed points:"),
       tableOutput('view'),
       downloadButton('downloadResults', 'Download Transformed Points')
